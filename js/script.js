cf3bad31b92010709bf7f012e4a83e484cb8d409
LESAINT225 <93989657+LESAINT225@users.noreply.github.com>
2023-11-21 18:23:50 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*============================= typing animation ==============================*/
+var typed = new Typed(".typing",{
+    strings:["","Web Desiner","Web Developer","Graphic Desiner","Youtuber"],
+    typespeed:9100,
+    BackSpeed:1600,
+    loop:true
+})
